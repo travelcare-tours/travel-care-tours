b77cfac0db39ab7921a88c596af2353c46ae572c
travelcare-tours <travelcare598@gmail.com>
2026-09-07 18:30:20 +0530

    Add Netlify Visual Editor configuration

---
+++ stackbit.config.ts
@@ -0,0 +1,69 @@
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "HomePage",
+          type: "page",
+          urlPath: "/",
+          filePath: "content/pages/home.json",
+          fields: [
+            { name: "heroEyebrow", type: "string" },
+            { name: "heroTitle", type: "string" },
+            { name: "heroHighlight", type: "string" },
+            { name: "heroDescription", type: "string", controlType: "textarea" },
+            { name: "packagesHeading", type: "string" },
+            { name: "packagesDescription", type: "string", controlType: "textarea" },
+            { name: "experienceEyebrow", type: "string" },
+            { name: "experienceTitle", type: "string" },
+            { name: "experienceDescription", type: "string", controlType: "textarea" },
+            { name: "destinationsHeading", type: "string" },
+            { name: "destinationsDescription", type: "string", controlType: "textarea" },
+            { name: "whyHeading", type: "string" },
+            { name: "ctaTitle", type: "string" },
+            { name: "ctaDescription", type: "string", controlType: "textarea" },
+            { name: "enquiryHeading", type: "string" },
+            { name: "enquiryDescription", type: "string", controlType: "textarea" }
+          ]
+        },
+        {
+          name: "Package",
+          type: "data",
+          filePath: "content/packages/{slug}.json",
+          fields: [
+            { name: "slug", type: "string" },
+            { name: "location", type: "string" },
+            { name: "tag", type: "string" },
+            { name: "title", type: "string" },
+            { name: "description", type: "string", controlType: "textarea" },
+            { name: "duration", type: "string" },
+            { name: "priceLabel", type: "string" },
+            { name: "image", type: "image" }
+          ]
+        },
+        {
+          name: "Destination",
+          type: "data",
+          filePath: "content/destinations/{slug}.json",
+          fields: [
+            { name: "slug", type: "string" },
+            { name: "name", type: "string" },
+            { name: "tagline", type: "string" },
+            { name: "image", type: "image" }
+          ]
+        }
+      ],
+      assetsConfig: {
+        referenceType: "relative",
+        assetsDir: "assets",
+        uploadDir: "images"
+      }
+    })
+  ]
+});
